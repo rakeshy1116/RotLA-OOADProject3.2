11b2e8e2a6edf7c0682e6ec2fc0a73d4b0a9b178
Rakesh Chowdary <rakeshchowdary@Rakeshs-MacBook-Air.local>
2022-09-17 21:02:17 -0600

    added creature

--- src/Adventurer.java
+++ src/Adventurer.java
@@ -1,9 +1,27 @@
 
-public class Adventurer {
+abstract public class Adventurer {
     private int damage;
     private int noOfDamages;
     private Room room;
 
+    public void performTurn(Dice dice) {
+            move();
+            fight(dice);
+            findTreasure(dice);
+        //TODO creature check to be added after room class created
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public boolean isAlive() {
+        return noOfDamages<3;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 
     Adventurer(Room room, int damage, int noOfDamages){
         this.damage=damage;
@@ -12,7 +30,12 @@ public class Adventurer {
     }
 
     public void move() {
+       // TODO will add the method after room is added
+    }
 
+    abstract public int fight(Dice dice);
+    public int findTreasure(Dice dice) {
+        return dice.getRandoms();
     }
 }
 
@@ -23,8 +46,51 @@ class Brawler extends Adventurer {
         super(room,damage,noOfDamages);
     }
 
-    public void move() {
+    public int fight(Dice dice) {
+        return 2 + dice.getRandoms();
+    }
 
+}
+
+class Runner extends Adventurer {
+
+    Runner(Room room, int damage, int noOfDamages) {
+        super(room,damage,noOfDamages);
+    }
+
+    public int fight(Dice dice) {
+        return 2 + dice.getRandoms();
+    }
+
+}
+
+class Sneaker extends Adventurer {
+
+    Sneaker(Room room, int damage, int noOfDamages) {
+        super(room,damage,noOfDamages);
+    }
+
+    public int fight(Dice dice) {
+        int min = 0;
+        int max = 1;
+        int chance= (int)Math.floor(Math.random()*(max-min+1)+min);
+        return chance * dice.getRandoms();
+    }
+
+}
+
+class Thieves extends Adventurer {
+
+    Thieves(Room room, int damage, int noOfDamages) {
+        super(room,damage,noOfDamages);
+    }
+
+    public int fight(Dice dice) {
+        return 1 + dice.getRandoms();
+    }
+
+    public int findingTreasure(Dice dice) {
+        return 1 + dice.getRandoms();
     }
 
 }
--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,5 +1,6 @@
 public class GameEngine {
     public static void main  (String args[]) {
-        System.out.println("Game");
+
+        System.out.println("Game Engine");
     }
 }
